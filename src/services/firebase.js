e4422e44651bf078450b5040b3fb29bbd9e0f442
Aurelien NEZZAR <aurelien.nezzar@outlook.fr>
2020-03-25 03:00:18 +0100

    Firebase added

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAGOyjkk8bS2cTccYyoKXbUKtE2T1t0LeU",
+    authDomain: "ponctual-fbaf7.firebaseapp.com",
+    databaseURL: "https://ponctual-fbaf7.firebaseio.com",
+    projectId: "ponctual-fbaf7",
+    storageBucket: "ponctual-fbaf7.appspot.com",
+    messagingSenderId: "991220401938",
+    appId: "1:991220401938:web:e8604c296a2c61c4e5d5cb"
+};
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();
