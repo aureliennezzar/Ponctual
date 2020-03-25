f898f25fbfc5368192b1f7b68e8afd92342b9715
Aurelien NEZZAR <aurelien.nezzar@outlook.fr>
2020-03-25 03:00:18 +0100

    Firebase added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Login from './pages/Login'
-// import firebase from 'services/firebase';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
