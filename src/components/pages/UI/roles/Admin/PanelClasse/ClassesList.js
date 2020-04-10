e0b236027bd4cdd1f059228f773c1ce1b816f65d
Aurelien NEZZAR <aurelien.nezzar@outlook.fr>
2020-04-10 02:30:57 +0200

    Admin panel

--- src/components/pages/UI/roles/Admin/PanelClasse/ClassesList.js
+++ src/components/pages/UI/roles/Admin/PanelClasse/ClassesList.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-
 import "./ClassesList.css"
 
 const ClassesList = props => {
-    useEffect(() => {
-        document.body.style.background = "white";
-    }, [])
     return (
-        <div>
-            Liste classes
-        </div>
+        <div>Liste Classes</div>
     )
 }
 export default ClassesList
--- src/components/pages/UI/roles/Admin/PanelClasse/ClassesToolbar.js
+++ src/components/pages/UI/roles/Admin/PanelClasse/ClassesToolbar.js
@@ -1,21 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../../../../../../scripts/services/firebase"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import "./ClassesList.css"
+import "./ClassesToolbar.css"
 
 
 const ClassesList = props => {
+    const [inputVal, setInputVal] = useState("")
     useEffect(() => {
         document.body.style.background = "white";
 
     }, [])
+    const handleChange = (event) => {
+        setInputVal(event.target.value)
+    }
     const handleClick = (event) => {
-        alert("create Class")
+        if (inputVal.length > 0) {
+            db.collection("classes").doc(inputVal).set({
+                nom: inputVal,
+                eleves: []
+            })
+                .then(function () {
+                    console.log("Document successfully written!");
+                })
+                .catch(function (error) {
+                    console.error("Error writing document: ", error);
+                });
+            setInputVal("")
+        }
     }
     return (
         <div className="toolbar">
             <h1 className="toolTitle">Gestion des classes</h1>
-            <input type="text" name="className" id="className" placeholder="Entrer nom" />
+            <input type="text" name="className" id="className" placeholder="Entrer nom" onChange={handleChange} value={inputVal} />
             <FontAwesomeIcon className="addBtn" icon={faPlus} size="4x" onClick={handleClick} />
         </div>
     )
