91d4349374b2d8594f956ebc0f844b4d2809d29b
Aurelien NEZZAR <aurelien.nezzar@outlook.fr>
2020-04-25 05:52:11 +0200

    StudentPanel: appointment settings made

--- src/components/pages/UI/roles/Student/Student.js
+++ src/components/pages/UI/roles/Student/Student.js
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { auth } from "../../../../../scripts/services/firebase";
+import { auth, db } from "../../../../../scripts/services/firebase";
 import "./Student.css"
-import { cleanup } from "@testing-library/react";
 
 const Student = props => {
   const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [state, setState] = useState({
+    user: "",
+    appointments: [],
+  })
   useEffect(() => {
     document.body.style.background = "#F4F7F6";
-    setUser(auth().currentUser)
+    initAppointments()
     setLoading(false)
     return () => {
       setLoading(true)
     }
   }, [])
+
+  const initAppointments = () => {
+    db.collection("users").doc(auth().currentUser.uid).get().then((doc) => {
+      db.collection("classes").doc(doc.data().classe).get().then((doc) => {
+        const { appointments } = doc.data()
+        setState({
+          ...state,
+          appointments
+        })
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }).catch(function (err) {
+      console.log(err)
+    })
+  }
+  console.log(state)
   return (
     <div className="studentPanel">
-      {loading === false && user && <h1>Bonjour, {user.displayName} Eleve</h1>}
+      {loading === false && state.user && <h1>Bonjour, {state.user.displayName}</h1>}
       <div className="nextCourseStudent">
-          <p>Votre prochain cours commence a xH</p>
+        <p>Votre prochain cours commence a xH</p>
         <div className="nextCourseInfosStudent">
           <p>Infos du cours</p>
         </div>
